29a6e1e9831a1007bfe6af62f0280320c78af3fa
skr0nks <108427178+skr0nks@users.noreply.github.com>
2022-12-13 14:17:25 +0300

    Update train02.js

--- train02.js
+++ train02.js
@@ -420,3 +420,28 @@ let arr = []
 
 console.log(mergeTwoLists([],[0]))
   
+20.Leetcode Valid Parentheses
+var isValid = function (inputString) {
+    // Initialise an array to store the closing brackets expected
+    let expectedBrackets = [];
+
+    // Loop through the letters in the input string
+    for (let letter = 0; letter < inputString.length; letter++) {
+        // Push the closing equivelant of any open brackets found
+        if (inputString[letter] == '{') {
+            expectedBrackets.push('}');
+        } else if (inputString[letter] == '[') {
+            expectedBrackets.push(']');
+        } else if (inputString[letter] == '(') {
+            expectedBrackets.push(')');
+        }
+        // If a close bracket is found, check that it matches the last stored open bracket
+        else if (expectedBrackets.pop() !== inputString[letter]) {
+            return false;
+        }
+    }
+
+    // Return based on whether or not anything remains
+    // (indicates that there were incorrect brackets)
+    return !expectedBrackets.length;
+};
